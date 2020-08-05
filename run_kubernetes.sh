a002462b10070a9998fafc0f45d042ca59e5e524
Roberto Meléndez <rcmelendez@users.noreply.github.com>
2020-08-05 12:07:25 -0400

    Deploy with Kubernetes and Save Output Logs
    Signed-off-by: Roberto Meléndez <rcmelendez@users.noreply.github.com>

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="rcmelendez/ml_predictions"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run predictions\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=predictions
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward predictions 8000:80
